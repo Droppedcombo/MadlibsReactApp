550dfeab20ddc3d67629b47cb748483ffb78d08a
benrojas <droppedcombo7@gmail.com>
2018-03-20 15:03:31 -0600

    First component

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import MadlibForm from "./madlib_form";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>DevCamp React Starter</h1>
+        <div className="backgroundSkew">
+         <div className="madlib-heading">
+          <h1>Bottega Mad Libs</h1>
+         <div className="madlib-subheading">
+            fill out the fields below and click the generate button<br/> to see the Mad Lib story.
+         </div>
+        </div>
+      <MadlibForm/>
+       </div>
       </div>
     );
   }
---
+++ src/components/madlib_form.js
@@ -0,0 +1,89 @@
+import React, {Component} from "react";
+import {
+    Col,
+    Row,
+    Card
+} from "reactstrap";
+import _ from "lodash";
+
+function MadlibInput(props) {
+    return (
+        <Col md="3" className="input-wrapper">
+                <Row>
+                        <Col md="2">
+                            <label className="color-label">{props.index}</label>
+                        </Col>
+                        <Col md="10">
+                            <input placeholder={props.placeholder} value={props.state} type="text" onChange={props.onChange}/>
+                        </Col>
+                </Row>
+                    <Row>
+                        <Col md="2"></Col>
+                        <Col md="10">
+                            <div className="input-description">{props.placeholder}</div>
+                        </Col>
+                    </Row>
+        </Col>
+        );
+    }
+
+
+class MadLibForm extends Component {
+   
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            color: "",
+            pluralNoun: "",
+            adjectiveOne: "",
+            celebrityOne: "",
+            adjectiveTwo: "",
+            nounOne: "",
+            numberOne: "",
+            numberTwo: "",
+
+        }
+    }
+    
+    handleChange = function(props) {
+        return function(event) {
+        console.log(`value for input ${props.inputTitle} is: ${event.target.value}`)
+        this.setState({[props.inputTitle]: event.target.value});
+        console.log(`value for state ${props.inputTitle} is: ${this.state[props.inputTitle]}`)
+        }.bind(this);
+    }
+
+    render() { 
+
+        this.inputData = [
+            {placeholder: 'Color', prop: 'color', state: this.state.color},
+            {placeholder: 'Noun (Plural)', prop: 'pluralNoun', state: this.state.pluralNoun},
+            {placeholder: 'Adjective', prop: 'adjectiveOne', state: this.state.adjectiveOne},
+            {placeholder: 'Celebrity', prop: 'celebrityOne', state: this.state.celebrityOne},
+
+            {placeholder: 'Adjective', prop: 'adjectiveTwo', state: this.state.adjectiveTwo},
+            {placeholder: 'Noun (Plural)', prop: 'nounOne', state: this.state.nounOne},
+            {placeholder: 'Number', prop: 'numberOne', state: this.state.numberOne},
+            {placeholder: 'Number', prop: 'numberTwo', state: this.state.numberTwo},
+           
+        ]      
+        
+        return (
+           <div className="card-wrapper">
+                <Card>
+                   <Row style={{textAlign: 'center', color: 'white'}}>
+                        {
+                                _.map(this.inputData, (data, indexKey) => {
+                                    return <MadlibInput key={indexKey} index={indexKey + 1} state={data.state} placeholder={data.placeholder} onChange={this.handleChange({inputTitle: data.prop})} />
+                                 }) 
+                        }
+                  </Row>
+                </Card>
+          </div>
+        );
+    }
+}
+
+
+export default MadLibForm;
